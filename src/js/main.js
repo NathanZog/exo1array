eb03802d1debe6026d35af03aaa56248caf663a0
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-03 13:45:21 +0200

    ajout exo 3-4

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,30 @@ console.log(coding)
 coding.push("Nathan")
 
 console.log(coding)
+
+// Exo array 3
+
+let alcohol = ["Whisky", "Rhum", "Tequila"]
+
+console.log(alcohol)
+
+alcohol.unshift("Rhum Brun pour Nathan")
+
+alcohol.splice(2,1,"Gin")
+
+console.log(alcohol)
+
+// Exo array 4
+
+let soiree = ["Deyuu", "Eluu", "Naytou"]
+
+console.log(soiree)
+
+soiree.unshift("Naytou")
+
+soiree.splice(3,1,"Agimou")
+
+soiree.pop()
+
+console.log(soiree)
+
