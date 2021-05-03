ce1819e43d9c0fe8a898dcbeeb76f723937a67e2
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-03 11:59:08 +0200

    exo array1

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+
+
+let legendary = ["Sulfura", "Artikodin", "Electhor"];
+
+console.log(legendary);
+
+console.log(legendary.length);
+
+console.log(legendary[0]);
+console.log(legendary[1]);
+console.log(legendary[2]);
