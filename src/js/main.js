cad65f5bc802668e90925818ef52e6a6232b4d47
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-03 12:10:16 +0200

    exo 1+ 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-
-
 let legendary = ["Sulfura", "Artikodin", "Electhor"];
 
 console.log(legendary);
@@ -9,3 +7,13 @@ console.log(legendary.length);
 console.log(legendary[0]);
 console.log(legendary[1]);
 console.log(legendary[2]);
+
+// Exo array 2
+
+let coding = ["coding17", "coding18", "coding19"]
+
+console.log(coding)
+
+coding.push("Nathan")
+
+console.log(coding)
